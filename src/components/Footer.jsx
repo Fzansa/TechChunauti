606ecaace8ca2eda709bebb114f7f024578b9638
Faizan Ansari <81568950+Fzansa@users.noreply.github.com>
2023-08-09 13:46:24 +0530

    TechChunauti

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div>
+        <h1>TechChunauti</h1>
+        <p>@all right reserved</p>
+      </div>
+
+      <div>
+        <h5>Follow Us</h5>
+        <div>
+          <a href="https://www.youtube.com/@LearnwithFz">Youtube</a>
+          <a href="https://instagram.com/learn_with_fz">Instagram</a>
+          <a href="https://github.com/fzansa">Github</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
